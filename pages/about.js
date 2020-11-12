d5a332f83a0be7f771686e56a7765bfb07d16cf7
Stanley Jeong <jeongstanley@gmail.com>
2020-11-11 16:24:03 -0800

    lineheight added to about and howtouse pages

--- pages/about.js
+++ pages/about.js
@@ -9,29 +9,31 @@ export default function About() {
         <title>About</title>
       </Head>
 
-      <blockquote>
-        <em>
-          "A rental property calculator / investment analytics tool helping real estate investors gain insights through property analysis, dynamically sourced tax rates, and a custom, comprehensive mortgage calculator"
-        </em>
-      </blockquote>
+      <div className="layoutBody">
+        <blockquote>
+          <em>
+            "A rental property calculator / investment analytics tool helping real estate investors gain insights through property analysis, dynamically sourced tax rates, and a custom, comprehensive mortgage calculator"
+          </em>
+        </blockquote>
 
-      <h1>About</h1>
-      <p>Realyzer is a multi-variable calculator designed for real estate investors to analyze a property's profitability.</p>
-      <p>
-        Realyzer was created to help real estate investors and professionals analyze residential 
-        and commercial properties, providing metrics in order to determine profitability. It analyzes 
-        and assesses any given property's net operating income and cash-on-cash return based on the property's 
-        value (the purchase price a.k.a. the home value), down payment, the potential rental income, and the 
-        overall expenses of the property, including mortgage payments, property taxes, property insurance, and more.
-      </p>
-      <h1>Realyzer:</h1>
-      <ul>
-        <li>Provides a clean and simple-to-use interface that keeps you focused on gaining your insights</li>
-        <li>Eliminates the need to visit external web sites and tools for property tax or mortgage calculations</li>
-        <li>Provides you the option to save/print a clean and simple PDF report of your property analysis</li>
-      </ul>
+        <h1>About</h1>
+        <p>Realyzer is a multi-variable calculator designed for real estate investors to analyze a property's profitability.</p>
+        <p>
+          Realyzer was created to help real estate investors and professionals analyze residential 
+          and commercial properties, providing metrics in order to determine profitability. It analyzes 
+          and assesses any given property's net operating income and cash-on-cash return based on the property's 
+          value (the purchase price a.k.a. the home value), down payment, the potential rental income, and the 
+          overall expenses of the property, including mortgage payments, property taxes, property insurance, and more.
+        </p>
+        <h1>Realyzer:</h1>
+        <ul>
+          <li>Provides a clean and simple-to-use interface that keeps you focused on gaining your insights</li>
+          <li>Eliminates the need to visit external web sites and tools for property tax or mortgage calculations</li>
+          <li>Provides you the option to save/print a clean and simple PDF report of your property analysis</li>
+        </ul>
 
-      <h4>For more information on how it was made: <a href="https://stanjeong.vercel.app/projects/realyzer" target="_blank">Case Study</a></h4>
+        <h4>For more information on how it was made: <a href="https://stanjeong.vercel.app/projects/realyzer" target="_blank">Case Study</a></h4>
+      </div>
 
     </Layout>
   )
--- pages/howToUse.js
+++ pages/howToUse.js
@@ -10,62 +10,65 @@ export default function HowToUse() {
         <title>How To Use</title>
       </Head>
 
-      <h1>How To Use</h1>
-      <p>
-        Realyzer was created for any level real estate investor, from the newbie investor to the experienced. This tool provides a simple and intuitive interface to input your data for any rental property.
-      </p>
+      <div className="layoutBody">
+        <h1>How To Use</h1>
+        <p>
+          Realyzer was created for any level real estate investor, from the newbie investor to the experienced. This tool provides a simple and intuitive interface to input your data for any rental property.
+        </p>
 
-      <div className={util.sectionSpacing}>
-        <h1>Visual Walkthrough</h1>
-        <img src="/gifs/realyzer-walkthrough.gif" style={{width:"100%"}} alt="realyzer rental property calculator walkthrough usage" />
-        <figcaption>Walkthrough usage of the app</figcaption>
-      </div>
-
-      <div className={util.sectionSpacing}>
-        <h1>Enter Property Address for Map and Property Tax Estimates:</h1>
-        <img src="/gifs/realyzer-mapbox-geocoding.gif" style={{width:"100%"}} className="center" alt="rental property calculator mapbox geocoding address to coordinates feature"/>
-        <figcaption>Enter an address and click "Search Address" to find the property on the map!</figcaption>
-      </div>
-
-
-      <div className={util.sectionSpacing}>
-        <div className="readingWidth">
-          <h1>Mortgage Calculation Formula</h1>
-          <p>
-            For the mortgage calculation, the key variables to keep track of were the Property Value, the user's Down Payment Percentage (e.g. 20% down), 
-            the Interest Rate (e.g. 4%), and the Length of the Loan (e.g. 30 years). These numbers were then plugged into a complex mortgage calculation formula 
-            to determine the user's monthly mortgage payment. The user then has the freedom to adjust the Down Payment and Interest Rate via range inputs
-            to dynamically adjust the monthly payments.
-          </p>
-
-          <pre className="formulas">
-            <code >
-{`  Mortgage calculation formula:
-  M = P [ r(1 + r)^n ] / [ (1 + r)^n – 1]
-    P = The Principal loan amount
-    r = Monthly Interest Rate
-    n = Number of months required to repay the loan
-`}
-            </code>
-          </pre>
-          <figure className="center">
-            <img src="/gifs/realyzer-mortgage-calc.gif" className="center" width="100%" alt="rental property calculator simple mortgage calculator"/>
-            <figcaption>Simple, Adjustable Mortgage Calculator</figcaption>
-          </figure>
+        <div className={util.sectionSpacing}>
+          <h1>Visual Walkthrough</h1>
+          <img src="/gifs/realyzer-walkthrough.gif" style={{width:"100%"}} alt="realyzer rental property calculator walkthrough usage" />
+          <figcaption>Walkthrough usage of the app</figcaption>
         </div>
-      </div>
 
-      <div className={util.sectionSpacing}>
-        <div className="readingWidth">
-          <h1>Final Results:</h1>
-          <img src="/pdf-results/pdf1.png" style={{width: "100%"}} alt="Realyzer rental property calculator PDF page 1 cash on cash return net cash flow" className="caseStudyImage center"/>
-          <img src="/pdf-results/pdf2.png" style={{width: "100%"}} alt="Realyzer rental property calculator PDF page 2 monthly expenses down payment closing costs" className="caseStudyImage center"/>
-          <img src="/pdf-results/pdf3.png" style={{width: "100%"}} alt="Realyzer rental property calculator PDF page 3 mapbox api zillow redfin map marker" className="caseStudyImage center"/>
-          <figcaption>Final result pages with your inputted data available to save as a PDF or print!</figcaption>
+        <div className={util.sectionSpacing}>
+          <h1>Enter Property Address for Map and Property Tax Estimates:</h1>
+          <img src="/gifs/realyzer-mapbox-geocoding.gif" style={{width:"100%"}} className="center" alt="rental property calculator mapbox geocoding address to coordinates feature"/>
+          <figcaption>Enter an address and click "Search Address" to find the property on the map!</figcaption>
         </div>
+
+
+        <div className={util.sectionSpacing}>
+          <div className="readingWidth">
+            <h1>Mortgage Calculation Formula</h1>
+            <p>
+              For the mortgage calculation, the key variables to keep track of were the Property Value, the user's Down Payment Percentage (e.g. 20% down), 
+              the Interest Rate (e.g. 4%), and the Length of the Loan (e.g. 30 years). These numbers were then plugged into a complex mortgage calculation formula 
+              to determine the user's monthly mortgage payment. The user then has the freedom to adjust the Down Payment and Interest Rate via range inputs
+              to dynamically adjust the monthly payments.
+            </p>
+
+            <pre className="formulas">
+              <code >
+  {`  Mortgage calculation formula:
+    M = P [ r(1 + r)^n ] / [ (1 + r)^n – 1]
+      P = The Principal loan amount
+      r = Monthly Interest Rate
+      n = Number of months required to repay the loan
+  `}
+              </code>
+            </pre>
+            <figure className="center">
+              <img src="/gifs/realyzer-mortgage-calc.gif" className="center" width="100%" alt="rental property calculator simple mortgage calculator"/>
+              <figcaption>Simple, Adjustable Mortgage Calculator</figcaption>
+            </figure>
+          </div>
+        </div>
+
+        <div className={util.sectionSpacing}>
+          <div className="readingWidth">
+            <h1>Final Results:</h1>
+            <img src="/pdf-results/pdf1.png" style={{width: "100%"}} alt="Realyzer rental property calculator PDF page 1 cash on cash return net cash flow" className="caseStudyImage center"/>
+            <img src="/pdf-results/pdf2.png" style={{width: "100%"}} alt="Realyzer rental property calculator PDF page 2 monthly expenses down payment closing costs" className="caseStudyImage center"/>
+            <img src="/pdf-results/pdf3.png" style={{width: "100%"}} alt="Realyzer rental property calculator PDF page 3 mapbox api zillow redfin map marker" className="caseStudyImage center"/>
+            <figcaption>Final result pages with your inputted data available to save as a PDF or print!</figcaption>
+          </div>
+        </div>
+
+        <h4>For more on how it was made: <a href="https://stanjeong.vercel.app/projects/realyzer" target="_blank">Case Study</a></h4>
       </div>
 
-      <h4>For more on how it was made: <a href="https://stanjeong.vercel.app/projects/realyzer" target="_blank">Case Study</a></h4>
 
     </Layout>
   )
