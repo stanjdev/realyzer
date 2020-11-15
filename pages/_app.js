207ea844dd63f74445d585637adf2076fed1b320
Stanley Jeong <jeongstanley@gmail.com>
2020-11-14 18:53:56 -0800

    google analytics added to nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
-/* eslint-disable react/react-in-jsx-scope */
 import '../styles/globals.css'
 import { Provider } from 'react-redux';
 import store from '../redux/store'
 import {LightComponent} from '../components/LightContext';
 
-function MyApp({ Component, pageProps }) {
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Provider store={store}>
       <LightComponent>
@@ -15,3 +29,38 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OG: 
+// /* eslint-disable react/react-in-jsx-scope */
+// import '../styles/globals.css'
+// import { Provider } from 'react-redux';
+// import store from '../redux/store'
+// import {LightComponent} from '../components/LightContext';
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Provider store={store}>
+//       <LightComponent>
+//         <Component {...pageProps} />
+//       </LightComponent>
+//     </Provider>
+//   )
+// }
+
+// export default MyApp
