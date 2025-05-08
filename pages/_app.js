55318b42f6823b42e2a65a97996b113fc4f2b6b8
Stan <61481150+stanjdev@users.noreply.github.com>
2025-05-08 16:11:47 -0700

    Update _app.js with Script tag for GA4
    added <Script> tag 5-8-25 for GA4 tracking

--- pages/_app.js
+++ pages/_app.js
@@ -3,37 +3,53 @@ import { Provider } from 'react-redux';
 import store from '../redux/store'
 import { LightComponent } from '../components/LightContext';
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter()
   useEffect(() => {
+    // Trigger pageview on route change
     const handleRouteChange = (url) => {
       gtag.pageview(url)
     }
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('routeChangeComplete', handleRouteChange);
     }
-  }, [router.events])
+  }, [router.events]);
 
   return (
-    <Provider store={store}>
-      <LightComponent>
-        <Component {...pageProps} />
-      </LightComponent>
-    </Provider>
-  )
+    <>
+      {/* Google Analytics scripts */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Provider store={store}>
+        <LightComponent>
+          <Component {...pageProps} />
+        </LightComponent>
+      </Provider>
+    </>
+  );
 }
-
-
-
-
-
-
-
 
 
 
